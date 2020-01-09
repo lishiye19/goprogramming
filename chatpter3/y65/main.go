6e886562534e1aa720e8fe1a6e24d7c1d8f5db86
lishiye19 <lishiye19@163.com>
2020-01-09 18:44:51 +0800

    Update main.go

--- chatpter3/y65/main.go
+++ chatpter3/y65/main.go
@@ -1,5 +1,7 @@
 package main
 
+import "log"
+
 type Integer int
 
 type Rect struct {
@@ -7,7 +9,39 @@ type Rect struct {
 	width, height float64
 }
 
+type Base struct {
+	Name string
+}
+
+type Foo struct {
+	*Base
+}
+
+type Job struct {
+	Comman string
+	*log.Logger
+}
+
+func (job *Job) Start() {
+
+}
+
+func (base *Base) Foo() {
+	base.Name = "jinru base foo"
+	println(base.Name)
+}
+
+func (base *Base) Bar() {
+
+}
+
+func (foo *Foo) Bar() {
+	foo.Base.Bar()
+}
+
 func main() {
+	var foo = Foo{new(Base)}
+	foo.Foo()
 	var b Integer = 2
 	var a *Integer = &b
 	a.Add(2)
@@ -20,6 +54,15 @@ func main() {
 	println("面积=", aa)
 	//var rect2 = new(Rect)
 	//var rect3 = &Rect{}
+	var map1 map[int]string
+	map1 = make(map[int]string)
+
+	map1[2] = "1234"
+	println("map1[2]=", map1[2])
+	value, ok := map1[2]
+	if ok {
+		println("map1[2]=", value)
+	}
 }
 
 func (a Integer) Add(b Integer) {
