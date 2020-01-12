ae2cc157c76f0ae26889cd181ac9bd4a5b8118a3
lishiye19 <lishiye19@163.com>
2020-01-12 21:28:48 +0800

    Update main.go

--- chatpter5/cp51/main.go
+++ chatpter5/cp51/main.go
@@ -1,5 +1,30 @@
 package main
 
-func main() {
-
-}
+//
+//import (
+//	"log"
+//	"net"
+//)
+//
+//func main() {
+//	addr, err := net.ResolveTCPAddr("tcp", "127.0.0.1:49999")
+//	if err != nil {
+//		log.Panicln(err.Error())
+//	}
+//	log.Println("ct5 listening at:", addr)
+//	tcp, err := net.ListenTCP("tcp", addr)
+//	if err != nil {
+//		log.Panicln(err.Error())
+//	}
+//	accept, err := tcp.Accept()
+//	if err != nil {
+//		log.Panicln(err.Error())
+//
+//	}
+//	for {
+//
+//		recbt := make([]byte, 1024)
+//		accept.Read(recbt)
+//	}
+//
+//}
