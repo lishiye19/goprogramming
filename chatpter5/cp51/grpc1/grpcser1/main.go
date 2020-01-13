bb4a5ee8e00ffffad1f8ca70ce7c2a280f7d2426
lishiye19 <lishiye19@163.com>
2020-01-13 21:29:15 +0800

    78978978
    YTYIGI

---
+++ chatpter5/cp51/grpc1/grpcser1/main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"github.com/golang/protobuf/protoc-gen-go"
+	"google.golang.org/grpc"
+)
+
+func main() {
+
+}
--- chatpter5/go.mod
+++ chatpter5/go.mod
@@ -1,3 +1,5 @@
 module chatpter5
 
 go 1.13
+
+require google.golang.org/grpc v1.26.0 // indirect
